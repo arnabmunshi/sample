4a3f7e8e85aa0b62fbd89a66b64f7f243ccf6b12
ARNAB MUNSHI <arnab47stars@gmail.com>
2018-08-10 15:04:08 +0530

    Update fleetzz.js

--- fleetzz.js
+++ fleetzz.js
@@ -1,39 +1,59 @@
+// @author arnabmunshi
 $(function() {
-  $('li.admin').addClass('active-page open');
-  $('li.admin ul.sub-menu').show();
-  $('li.admin ul.sub-menu li').addClass('animation');
-  $('li.animation a[href="admin-profile"]').addClass('active');
-  $('li.animation a[href="admin-profile"]').parent('li').addClass('active-bar');
+  $('li.operator').addClass('active-page open');
+  $('li.operator ul.sub-menu').show();
+  $('li.operator ul.sub-menu li').addClass('animation');
+  $('li.animation a[href="sos"]').addClass('active');
+  $('li.animation a[href="sos"]').parent('li').addClass('active-bar');
 
-  $('#form_admin_update').submit(function(e) {
+  $('.date-picker').datepicker({
+    orientation: "top auto",
+    autoclose: true
+  });
+
+  // operator attendance entry
+  $('#form_other_application_submit').submit(function(e) {
     e.preventDefault();
 
-    var form = $('#form_admin_update');
-    var submit_button = $('#form_admin_update button');
-    var redirect_url = './admin-profile';
+    var form = $('#form_other_application_submit');
+    var submit_button = $(this).find('#btnSubmit');
+    var submit_data = (form).serialize();
+    var submit_url = 'models/other-application/ajax_other_application_submit.php';
+    var redirect_url = './other-application';
+    var success_response_msg = 'Submited';
 
     $.ajax({
       type: 'POST',
-      url: 'models/ajax_update_admin.php',
-      data: (form).serialize(),
+      url: submit_url,
+      data: submit_data,
       context: this,
       beforeSend: function() {
         (submit_button).attr('disabled', true).html('<i class="fa fa-spinner fa-spin fa-fw"></i> Please Wait ...');
       },
       error: function(xhr) {
         console.log('Error Message: '+xhr.status);
+        // $('#doc-details .after-submit.msg-red').text('Server Error.').show();
+        // $('#form_create_division .button').attr('disabled', false).html('<i class="fa fa-plus-circle"></i> Add Doctor');
       },
       success: function(response) {
         console.log('Success Message: '+response);
         if (response === 'SESSION-OUT') {
           window.location = "./";
-        } else if (response.indexOf('Could not connect to server') >= 0) {
-        } else if (response == 'Updated') {
-          (submit_button).html('<i class="fa fa-check-circle"></i> Profile Updated');
-          setTimeout(function() {
-            window.location = redirect_url;
-          }, 1000);
-        }
+        } else {
+          if (response.indexOf('Could not connect to server') >= 0) {
+            // $('#doc-details .after-submit.msg-red').text('Detabase Connection Error.').show();
+            // $('#doc-details .button').html('<i class="fa fa-plus-circle"></i> Add Doctor').attr('disabled', false);
+          } else if (response == 'Saved') {
+            (form)[0].reset();
+            (submit_button).html('<i class="fa fa-check-circle"></i> '+success_response_msg);
+            setTimeout(function() {
+              window.location = redirect_url;
+            }, 1000);
+          } else {
+            $(submit_button).html('Submit').attr('disabled', false);
+            // $('#form_add_emp .after-submit.msg-red').text(response).show();
+          }
+        } // else
       }, // end success
     }); // end ajax
   });
