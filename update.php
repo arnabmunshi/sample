b8f61f2e48c6d9cf2d9f344134a81616e27f5c64
ARNAB MUNSHI <arnab47stars@gmail.com>
2018-09-25 20:37:25 +0530

    Create update.php

---
+++ update.php
@@ -0,0 +1,10 @@
+$sql = "UPDATE payable_master
+        SET payable_amount = '$payable_amount',
+            payable_int_amount = '$payable_int_amount'
+        WHERE customer_id = '{$_POST['customer_id']}'";
+        
+if(mysqli_query($dbcon, $sql)) {
+  echo 'Saved';
+} else {
+  echo 'Error: '.mysqli_error($dbcon);
+}
