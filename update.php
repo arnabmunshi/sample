f0de3cbd8d20d3388c4a2cc982e98ebacabba6a4
ARNAB MUNSHI <arnab47stars@gmail.com>
2018-10-10 01:25:26 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,8 +1,9 @@
 <?php
-$sql = "UPDATE payable_master
-        SET payable_amount = '$payable_amount',
-            payable_int_amount = '$payable_int_amount'
-        WHERE customer_id = '{$_POST['customer_id']}'";
+// payable_master pm
+$sql = "UPDATE payable_master pm
+  SET pm.payable_amount = '$payable_amount',
+      pm.payable_int_amount = '$payable_int_amount'
+  WHERE pm.customer_id = '{$_POST['customer_id']}'";
         
 if(mysqli_query($dbcon, $sql)) {
   echo 'Saved';
