f8d18abf99c1d09a431e4420bcdc54c68cd2dc2e
ARNAB MUNSHI <arnab47stars@gmail.com>
2018-10-14 12:03:36 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,12 +1,24 @@
 <?php
-// payable_master pm
-$sql = "UPDATE payable_master pm
-  SET pm.payable_amount = '$payable_amount',
-      pm.payable_int_amount = '$payable_int_amount'
-  WHERE pm.customer_id = '{$_POST['customer_id']}'";
-        
+require_once __DIR__.'/../../includes/ajax-session-check.php';
+
+$customer_id = $_POST['customer_id'];
+unset($_POST['customer_group_id']);
+unset($_POST['customer_id']);
+
+$update_string = [];
+foreach ($_POST as $key => $value) {
+  $string = $key."='".mysqli_real_escape_string($dbcon, $value)."'";
+  array_push($update_string, $string);
+};
+
+$data_set = implode(", ", $update_string);
+
+$sql = "UPDATE customer_master SET $data_set WHERE customer_id = $customer_id";
+// echo $sql;
+// die();
+
 if(mysqli_query($dbcon, $sql)) {
-  echo 'Saved';
+  echo 'Updated';
 } else {
   echo 'Error: '.mysqli_error($dbcon);
 }
