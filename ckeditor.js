b6d398e34f5821368819afe39b0ed136f69dffc8
ARNAB MUNSHI <arnab47stars@gmail.com>
2020-03-31 03:08:40 +0530

    Update ckeditor.js

--- ckeditor.js
+++ ckeditor.js
@@ -3,3 +3,9 @@
 CKEDITOR.replace( 'blogDetails' );
 var blogDetails = CKEDITOR.instances.blogDetails.getData();
 CKEDITOR.instances.editblogDetails.setData($(this).data('blog_details'));
+
+// get ckeditor value by class
+var ckID = $(this).find('.cke').attr('id').substring(4);
+var ckFun = 'CKEDITOR.instances.'+ckID+'.getData()';
+var cost_wise_accommodation = eval(ckFun);
+data.append('cost_wise_accommodation', cost_wise_accommodation);
